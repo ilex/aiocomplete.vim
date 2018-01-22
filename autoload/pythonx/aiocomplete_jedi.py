fbcaf4f18bd3920b28af61567d2507ce18b14b1b
ilex <ilexhostmaster@gmail.com>
2018-01-22 22:10:57 +0200

    Add preloading modules coroutine

--- autoload/pythonx/aiocomplete_jedi.py
+++ autoload/pythonx/aiocomplete_jedi.py
@@ -1,5 +1,6 @@
 import asyncio
 import jedi
+import pkgutil
 try:
     import ujson as json
 except ImportError:
@@ -42,12 +43,18 @@ async def handle_echo(reader, writer):
     print("Close the client socket")
     writer.close()
 
+
+async def preload_modules():
+    for (_, module, _) in pkgutil.iter_modules():
+        jedi.preload_module(module)
+        await asyncio.sleep(0.1)
+
 loop = asyncio.get_event_loop()
 coro = asyncio.start_server(handle_echo, '127.0.0.1', 8888, loop=loop)
-server = loop.run_until_complete(coro)
+server, _ = loop.run_until_complete(asyncio.gather(coro, preload_modules()))
 
 # Serve requests until Ctrl+C is pressed
-print('Serving on {}'.format(server.sockets[0].getsockname()))
+# print('Serving on {}'.format(server.sockets[0].getsockname()))
 try:
     loop.run_forever()
 except KeyboardInterrupt:
